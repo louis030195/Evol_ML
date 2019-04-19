799ffede11922b6994ea1e6df8cd347fd7b5e1fd
louis030195 <louis.beaumont@gmail.com>
2019-04-19 11:11:14 +0200

    Init repo

---
+++ Evol_ML/Assets/Scripts/Carnivorous.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Evol
+{
+    public class Carnivorous : LivingBeing
+    {
+
+        public Carnivorous(int life, int age, int lifeExpectancy, int satiety, int tiredness, int speed)
+            : base(life, age, lifeExpectancy, satiety, tiredness, speed)
+        {
+
+        }
+    }
+}
---
+++ Evol_ML/Assets/Scripts/Herbivorous.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Evol
+{
+    public class Herbivorous : LivingBeing
+    {
+
+        public Herbivorous(int life, int age, int lifeExpectancy, int satiety, int tiredness, int speed)
+            : base(life, age, lifeExpectancy, satiety, tiredness, speed)
+        {
+
+        }
+    }
+}
